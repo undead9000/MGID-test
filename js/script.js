f0fc5a82c849f644c3d37b1a0d9c61e255d3b625
Undead9000 <undead10000@gmail.com>
2019-08-15 00:04:23 +0300

    Fixes

--- js/script.js
+++ js/script.js
@@ -9,11 +9,21 @@
             nextArrow: $('.slider__next'),
         });
         $("#form-select").chosen({
-            disable_search: true
+            disable_search: true,
         });
         $('.header__menu').slicknav({
             label: '',
             appendTo: 'header .wrapper'
+        });
+        $('#form-select').on('chosen:showing_dropdown', function() {
+            $('.chosen-results:visible').mCustomScrollbar({
+                theme: 'minimal-dark'
+            });
+        });
+        $('#form-select').on('chosen:hiding_dropdown', function() {
+            if($(".chosen-results").length) {
+                $('.chosen-results').mCustomScrollbar('destroy');
+            }
         });
     });
 })(jQuery, this);
